bf16b630df4b320ce2d7fc3c907e40724d3ae5a8
Aaron Lesserson <57720571+alesserson94@users.noreply.github.com>
2026-03-31 22:23:37 -0400

    Add files via upload

--- basketball.sql
+++ basketball.sql
@@ -1,9 +1,3 @@
-CREATE TABLE users (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(100)  NOT NULL,
-    email      VARCHAR(150)  NOT NULL UNIQUE,
-    age        INT,
-    created_at DATETIME      DEFAULT CURRENT_TIMESTAMP
-);
+select * from all_seasons;
 
-select * from users;
+rename table all_seasons_clean to all_seasons;
